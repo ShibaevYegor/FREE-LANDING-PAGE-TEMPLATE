9864d1ee9e95f4b5f64bc058a65749dde1f880db
Yegor Shibaev <egorshibzik@gmail.com>
2022-11-20 14:19:36 +0500

    I am new changes

--- js/main.js
+++ js/main.js
@@ -23,3 +23,108 @@ function transformThree() {
 function transformFour() {
     dribble.classList.toggle("action")
 }
+
+
+
+
+
+let five = document.querySelector(".under__img-photoshop")
+let six = document.querySelector(".under__img-illustrator")
+let seven = document.querySelector(".under__img-after")
+let eight = document.querySelector(".under__img-figma")
+
+let ps = document.querySelector(".ps")
+let ai = document.querySelector(".ai")
+let ae = document.querySelector(".ae")
+let fagma = document.querySelector(".figma__img")
+
+function transformFive() {
+    ps.classList.toggle("action")
+}
+
+function transformSix() {
+    ai.classList.toggle("action")
+}
+
+function transformSeven() {
+    ae.classList.toggle("action")
+}
+
+function transformEight() {
+    fagma.classList.toggle("action")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// By @AbubakerSaeed96 (Twitter)
+// ===================
+
+// Inspiration:
+// Oleg Frolov's dribbble shot: https://dribbble.com/shots/5331825-Loading-XXI
+
+// I thought making this would be easy ...but boy 😩
+
+// Thank You For Viewing!
+// Say 👋🏻 or hire me 👉🏻 abubaker.saeed.1996@gmail.com
+// ================================================================
+
+// Helper(s)
+// =========
+let $ = (e) => document.querySelector(e);
+
+// Dots
+// ====
+let dots = $(".dots");
+
+// Function
+// ========
+function animate(element, className) {
+    element.classList.add(className);
+    setTimeout(() => {
+        element.classList.remove(className);
+        setTimeout(() => {
+            animate(element, className);
+        }, 500);
+    }, 2500);
+}
+
+// Execution
+// =========
+animate(dots, "dots--animate");
+
+// Professional Abubaker ;P
+// =====================
+const twitter = $(".abs-twitter");
+window.addEventListener(
+    "mousemove",
+    () => twitter.classList.add("abs-twitter--show"), { once: true }
+);
+
+
+
+
+
+
+let bb = document.querySelector(".bb")
+window.addEventListener("DOMContentLoaded", function(e) {
+    setTimeout(() => {
+        bb.classList.add("ss")
+        setTimeout(() => {
+            bb.style.display = "none"
+        }, 500)
+    }, 5000)
+})

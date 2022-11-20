edd1e8c80e72b7a15d6990d67f04b860fbe596c1
Yegor Shibaev <egorshibzik@gmail.com>
2022-11-20 13:11:20 +0500

    I am new changes

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let one = document.querySelector(".linkedin")
+let two = document.querySelector(".instagram")
+let three = document.querySelector(".behance")
+let four = document.querySelector(".dribble")
+
+let linkedin = document.querySelector(".in")
+let instagram = document.querySelector(".insta")
+let behance = document.querySelector(".be")
+let dribble = document.querySelector(".ball")
+
+function transformOne() {
+    linkedin.classList.toggle("action")
+}
+
+function transformTwo() {
+    instagram.classList.toggle("action")
+}
+
+function transformThree() {
+    behance.classList.toggle("action")
+}
+
+function transformFour() {
+    dribble.classList.toggle("action")
+}
